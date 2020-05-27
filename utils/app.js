a87080d3a95986292daa9509945d9155a060fc8b
Patrick Pontes <patrickhisoka@gmail.com>
2020-05-27 12:47:14 -0300

    first commit

---
+++ utils/app.js
@@ -0,0 +1,19 @@
+
+const PARAGRAPH = document.querySelector(".final-ideas");
+
+const fetchAPIIdeas = ()=>{
+    let PARTICIPANTS = document.querySelector(".participants").value;
+    const URL_TO_API = `http://www.boredapi.com/api/activity?participants=${PARTICIPANTS}`;
+
+    fetch(URL_TO_API)
+    .then(response => response.json()) // retorna uma promise  
+    .then(result => { 
+        PARAGRAPH.innerHTML = result.activity;
+        PARAGRAPH.style.display = "block";
+    })
+    .catch(err => { 
+    // trata se alguma das promises falhar
+        console.error('Failed retrieving information', err); 
+        alert("Um erro ocorreu durante o processamento de sua solicitação.");
+    });    
+}
